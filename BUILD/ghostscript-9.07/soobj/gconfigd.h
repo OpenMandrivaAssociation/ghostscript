06c24ded2c8690f0b418846e8d00046e302a538b
Bernhard Rosenkränzer <bero@lindev.ch>
2013-06-15 20:45:04 +0200

    Remove accidentally added files

--- BUILD/ghostscript-9.07/soobj/gconfigd.h
+++
@@ -1,8 +0,0 @@
-#define GS_LIB_DEFAULT "/usr/share/ghostscript/9.07/Resource/Init:/usr/share/ghostscript/9.07/lib:/usr/share/ghostscript/9.07/Resource/Font:/usr/share/ghostscript/fonts:/usr/share/fonts/default/ghostscript:/usr/share/fonts/default/type1:/usr/share/ghostscript/fonts:/usr/share/ghostscript/9.07/Resource:/usr/share/ghostscript/Resource:/usr/share/ghostscript/CIDFont:/usr/share/fonts/ttf:/usr/share/fonts/type1:/usr/share/fonts/default/Type1"
-#define GS_DEV_DEFAULT "x11alpha bbox"
-#define GS_CACHE_DIR "~/.ghostscript/cache/"
-#define SEARCH_HERE_FIRST 0
-#define GS_DOCDIR "/usr/share/doc/ghostscript-doc-9.07"
-#define GS_INIT "gs_init.ps"
-#define GS_REVISION 907
-#define GS_REVISIONDATE 20130214
